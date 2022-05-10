30bb0c4ba9cb824451ca25ec8b8f1254d3c51f1c
kosinaz <kosinaz@gmail.com>
2022-05-10 21:45:25 +0200

    attach the ball to the paddle

---
+++ src/game/Paddle.ts
@@ -0,0 +1,32 @@
+import Phaser from 'phaser'
+
+export default class Paddle extends Phaser.Physics.Matter.Image 
+{
+  private ball?: Phaser.Physics.Matter.Image
+  constructor(world: Phaser.Physics.Matter.World, x: number, y: number, texture: string, config?: Phaser.Types.Physics.Matter.MatterBodyConfig)
+  {
+    super(world, x, y, texture, undefined, config)
+    world.scene.add.existing(this)
+  }
+
+  attachBall (ball: Phaser.Physics.Matter.Image)
+  {
+    this.ball = ball
+    this.ball.x = this.x
+    this.ball.y = this.y - this.height * 0.5 - this.ball.height * 0.5
+    this.ball.setVelocity(0, 0)
+  }
+
+  update(cursors: Phaser.Types.Input.Keyboard.CursorKeys)
+  {
+    const speed = 10
+    if (cursors.left?.isDown) {
+      this.x -= speed
+    } else if (cursors.right?.isDown) {
+      this.x += speed
+    }
+    if (this.ball) {
+      this.ball.x = this.x
+    }
+  }
+}
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,25 +1,39 @@
 import Phaser from 'phaser';
+import Paddle from '../game/Paddle'
 
 export default class Game extends Phaser.Scene
 {
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
+  private paddle!: Paddle
   constructor()
   {
     super('game')
+  }
+
+  init()
+  {
+    this.cursors = this.input.keyboard.createCursorKeys()
   }
 
   create()
   {
     const { width, height } = this.scale
 
-    const ball = this.matter.add.image(400, 300, 'ball')
-    ball.setVelocity(10, 10)
+    const ball = this.matter.add.image(400, 300, 'ball', undefined, {
+      circleRadius: 10,
+    })
     ball.setFriction(0, 0)
     ball.setBounce(1)
     const body = ball.body as MatterJS.BodyType;
     this.matter.body.setInertia(body, Infinity)
 
-    const paddle = this.matter.add.image(width * 0.5, height * 0.9, 'paddle', undefined, {
+    this.paddle = new Paddle(this.matter.world, width * 0.5, height * 0.9, 'paddle', {
       isStatic: true
     })
+    this.paddle.attachBall(ball)
+  }
+
+  update(t: number, dt: number) {
+    this.paddle.update(this.cursors)
   }
 }
